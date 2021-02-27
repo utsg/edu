c89abed4eaafb46e1b28880983cf987c972c29b3
Kirill Tokarev <tokarevki2997@gmail.com>
2021-02-28 00:45:51 +0300

    Add ConvertToCamelCase

---
+++ java/src/main/java/kyu6/ConvertToCamelCase.java
@@ -0,0 +1,13 @@
+package kyu6;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class ConvertToCamelCase {
+    static String toCamelCase(String str) {
+        String[] words = str.split("[-_]");
+        return Arrays.stream(words, 1, words.length).map(
+                i -> (i.substring(0, 1).toUpperCase() + i.substring(1))
+        ).reduce(words[0], String::concat);
+    }
+}
---
+++ java/src/test/java/kyu6/ConvertToCamelCaseTest.java
@@ -0,0 +1,20 @@
+package kyu6;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConvertToCamelCaseTest {
+    @Test
+    public void testSomeUnderscoreLowerStart() {
+        String input = "the_Stealth_Warrior";
+        System.out.println("input: "+input);
+        assertEquals("theStealthWarrior", ConvertToCamelCase.toCamelCase(input));
+    }
+    @Test
+    public void testSomeDashLowerStart() {
+        String input = "the-Stealth-Warrior";
+        System.out.println("input: "+input);
+        assertEquals("theStealthWarrior", ConvertToCamelCase.toCamelCase(input));
+    }
+}
