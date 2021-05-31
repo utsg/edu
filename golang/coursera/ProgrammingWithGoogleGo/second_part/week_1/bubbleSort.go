1105c9d703c347eaa33f5f97f80a1229b38d85f0
Токарев Кирилл Игоревич <KITokarev@sberbank.ru>
2021-05-31 12:11:18 +0300

    Add stdin array input

--- golang/coursera/ProgrammingWithGoogleGo/second_part/week_1/bubbleSort.go
+++ golang/coursera/ProgrammingWithGoogleGo/second_part/week_1/bubbleSort.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func Swap(a int, b int) (int, int) {
-	bubble := a
-	a = b
-	b = bubble
-	return a, b
+	return b, a
 }
 
 func BubbleSort(array *[]int) {
@@ -21,9 +22,29 @@ func BubbleSort(array *[]int) {
 	}
 }
 
+func InputArray() []int {
+	input := bufio.NewReader(os.Stdin)
+	bytes, _, err := input.ReadLine()
+	if err != nil {
+		fmt.Println("Error", err)
+		os.Exit(1)
+	}
+
+	stringFromBytes := strings.Split(string(bytes), " ")
+	result := make([]int, len(stringFromBytes))
+	for ind, el := range stringFromBytes {
+		result[ind], err = strconv.Atoi(el)
+		if err != nil {
+			fmt.Println("Conversion error", err)
+			os.Exit(1)
+		}
+	}
+	return result
+}
+
 func main() {
-	unsorted := []int{5, 4, 3, 2, 1}
-	fmt.Println("Unsorted:", unsorted)
+	fmt.Print("Input ints splitted by space\nUnsorted: ")
+	unsorted := InputArray()
 
 	BubbleSort(&unsorted)
 	fmt.Println("Sorted:", unsorted)
