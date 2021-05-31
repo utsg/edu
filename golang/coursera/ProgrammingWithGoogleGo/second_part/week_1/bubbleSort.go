0a07df661651a89ed7b64a6ba8c7851414e75e1c
Токарев Кирилл Игоревич <KITokarev@sberbank.ru>
2021-05-31 11:14:39 +0300

    Add Swap() func

--- golang/coursera/ProgrammingWithGoogleGo/second_part/week_1/bubbleSort.go
+++ golang/coursera/ProgrammingWithGoogleGo/second_part/week_1/bubbleSort.go
@@ -4,20 +4,25 @@ import (
 	"fmt"
 )
 
+func Swap(a int, b int) (int, int) {
+	bubble := a
+	a = b
+	b = bubble
+	return a, b
+}
+
 func BubbleSort(array *[]int) {
-	for i := 0; i < len(*array) - 1; i++ {
+	for i := 0; i < len(*array)-1; i++ {
 		for j := i + 1; j < len(*array); j++ {
 			if (*array)[j] < (*array)[i] {
-				bubble := (*array)[i]
-				(*array)[i] = (*array)[j]
-				(*array)[j] = bubble
+				(*array)[i], (*array)[j] = Swap((*array)[i], (*array)[j])
 			}
 		}
 	}
 }
 
 func main() {
-	unsorted := []int{5,4,3,2,1}
+	unsorted := []int{5, 4, 3, 2, 1}
 	fmt.Println("Unsorted:", unsorted)
 
 	BubbleSort(&unsorted)
