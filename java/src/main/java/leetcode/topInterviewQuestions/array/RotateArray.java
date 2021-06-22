0668aa2b99146ae10a4e014962eb5faf14217fa1
Kirill Tokarev <tokarevki2997@gmail.com>
2021-06-23 02:12:34 +0300

    Add RotateArray Java Implementation (but with time limit exceeded)

--- java/src/main/java/leetcode/topInterviewQuestions/array/RotateArray.java
+++ java/src/main/java/leetcode/topInterviewQuestions/array/RotateArray.java
@@ -2,14 +2,11 @@ package leetcode.topInterviewQuestions.array;
 
 public class RotateArray {
     public int[] rotate(int[] nums, int k) {
-        if (k > 0) {
-            int last = nums[nums.length - 1];
-            for (int i = nums.length -1; i > 0; i--) {
-                nums[i] = nums[i-1];
-            }
-            nums[0] = last;
-            return rotate(nums, (k-1));
+        int[] a = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            a[(i+k) % nums.length] = nums[i];
         }
+        System.arraycopy(a, 0, nums, 0, nums.length);
         return nums;
     }
 }
